fc58c7219d8ce3dc625dcf80a80ff43cd6ab107c
unknown <theking920602@gmail.com>
2024-01-27 21:27:04 -0600

    Update Index.jsx

--- src/Components/Hero/Index.jsx
+++ src/Components/Hero/Index.jsx
@@ -32,22 +32,18 @@ const Hero = () => {
                     <HeroTitle>BABY TON</HeroTitle>
                     <HeroP>
                     Pepe is tired of watching everyone play hot potato with the endless derivative ShibaCumGMElonKishuTurboAssFlokiMoon Inu coins. The Inu’s have had their day. It’s time for the most recognizable meme in the world to take his reign as king of the memes. Pepe is here to make memecoins great again. Launched stealth with no presale, zero taxes, LP burnt and contract renounced, $PEPE is a coin for the people, forever. Fueled by pure memetic power, let $PEPE show you the way.
-                    </HeroP>
-                    
-
-                  
-
+                    </HeroP>                                  
                     <HeroBtnWrapper>
-                        <Link to={{ pathname: "https://app.ston.fi/swap" }} target="_blank">
-                            <Button
-                                onMouseEnter={onHover}
-                                onMouseLeave={onHover}
-                                primary="true"
-                                dark="true"
-                            >
-                                Buy Now {hover ? <ArrowForward /> : <ArrowRight />}
-                            </Button>
-                        </Link>
+                        <a 
+                            href="https://app.ston.fi/swap" 
+                            onMouseEnter={onHover} 
+                            onMouseLeave={onHover} 
+                            primary="true" 
+                            dark="true"
+                            style={{ textDecoration: 'none' }}
+                        >
+                            Buy Now {hover ? <ArrowForward /> : <ArrowRight />}
+                        </a>
                     </HeroBtnWrapper>
                 </HeroContent>
             </HeroContainer>
