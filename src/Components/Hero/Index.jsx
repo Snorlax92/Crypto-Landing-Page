e88b1a472ff8ae3a55a33060c28e9401b28f0b1a
unknown <theking920602@gmail.com>
2024-01-27 21:12:27 -0600

    Update Index.jsx

--- src/Components/Hero/Index.jsx
+++ src/Components/Hero/Index.jsx
@@ -28,7 +28,7 @@ const Hero = () => {
                     <VideoBg autoPlay loop muted src={Video} type='video/mp4' />
                 </HeroBg>
                 <HeroContent>
-                    <HeroTitle>Este es</HeroTitle>
+                    <HeroTitle>BABY TON</HeroTitle>
                     <HeroP>
                     Pepe is tired of watching everyone play hot potato with the endless derivative ShibaCumGMElonKishuTurboAssFlokiMoon Inu coins. The Inu’s have had their day. It’s time for the most recognizable meme in the world to take his reign as king of the memes. Pepe is here to make memecoins great again. Launched stealth with no presale, zero taxes, LP burnt and contract renounced, $PEPE is a coin for the people, forever. Fueled by pure memetic power, let $PEPE show you the way.
                     </HeroP>
